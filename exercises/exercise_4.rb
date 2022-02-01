27ed32a98ad4549a1ab19b476e0acacbc4f00d9f
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-02-01 04:45:42 +0000

    Add > and loop

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,24 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+puts "------- HERE THE NUMBERS OF THE STORES ----------"
+puts @mens_stores.count(:name)
+
+
+puts "=========== LOOP 1=========="
+@mens_stores.each do |elem|
+  puts elem.name
+  puts elem.annual_revenue
+end
+
+puts "=========== LOOP 2=========="
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+@womens_stores.each do |elem|
+  puts elem.name
+end
